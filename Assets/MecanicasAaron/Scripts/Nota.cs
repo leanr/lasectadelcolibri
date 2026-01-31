9be701bd274fcbcc13bd049721943177ab65ff14
Norien-euh <norah2006norah@outlook.com>
2026-01-31 15:18:58 +0100

    añadido prefab llave

---
+++ Assets/MecanicasAaron/Scripts/Nota.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Nota : Interactuable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public override void Usar(PlayerControllerCopy p)
+    {
+        p.Recoger(this.gameObject);
+        this.gameObject.SetActive(false);
+        Debug.Log("Llave recogida");
+        foreach (GameObject e in p.objetosRecogidos)
+        {
+            //Debug, necesito que imprima todas las clases de los objetos, como lo hago?
+            Debug.Log(e.GetType());
+        }
+    }
+}
